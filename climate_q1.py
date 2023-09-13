912628bce61592ff3887d00633ddffa74a3fad0c
kstrepo1 <kail.terepai@griffithuni.edu.au>
2023-09-13 21:18:51 +1000

    Adding code to import data from a SQL database using SQLITE, then converting the data from the database into usable data for the charts.

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,29 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3 as sq
+
+connection = sq.connect('climate.db')
+
+print(connection.total_changes)
+
+sql_query = """SELECT * FROM climateData;"""
+cursor = connection.cursor()
+cursor.execute(sql_query)
+
+result = cursor.fetchall()
+
 years = []
 co2 = []
 temp = []
+
+for a in result:
+    years.append(a[0])
+    co2.append(a[1])
+    temp.append(a[2])
+
+if connection:
+    connection.close()
+    print("The SQL connection is closed")
+
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
